5cf42eb288345ae7cbf55f6ff25f0cf0ffcc2ca8
Pedro A. Aranda <paaguti@gmail.com>
2026-01-26 07:30:50 +0100

    New option (org-latex-use-sans): Control using Sans as default font
    This is the second version, providing for #+OPTION: latex-use-sans:t
    in addition to configuring through variables.
    
    * lisp/ox-latex.el: (org-latex-use-sans) : new defcustom
      (org-latex-make-preamble): add command to typeset using the Sans
      family after LATEX_HEADER_EXTRA if not a snippet
      and (org-latex-use-sans) is not nil
    * testing/lisp/test-ox-latex.el: Two tests for (org-latex-use-sans)
    * doc/org-manual.org: document (org-latex-use-sans)
    * etc/ORG-NEWS: announce (org-latex-use-sans)

--- lisp/ox-latex.el
+++ lisp/ox-latex.el
@@ -170,6 +170,7 @@
     (:latex-toc-command nil nil org-latex-toc-command)
     (:latex-compiler "LATEX_COMPILER" nil org-latex-compiler)
     (:latex-multi-lang "LATEX_MULTI_LANG" nil org-latex-multi-lang)
+    (:latex-use-sans nil "latex-use-sans" org-latex-use-sans)
     ;; Redefine regular options.
     (:date "DATE" nil "\\today" parse)))
 
@@ -2664,6 +2665,15 @@ INFO is a plist used as a communication channel."
 	 (member (or compiler "") org-latex-compilers)
 	 (format org-latex-compiler-file-string compiler))))
 
+(defcustom org-latex-use-sans nil
+  "Whether to typeset the document with the Sans font family.
+
+The default behaviour is to typeset with the Roman font family."
+  :group 'org-export-latex
+  :package-version '(Org . "9.8")
+  :type 'boolean
+  :safe #'booleanp)
+
 
 ;;; Filters
 
@@ -2718,7 +2728,11 @@ specified in `org-latex-default-packages-alist' or
 	     (mapconcat #'org-element-normalize-string
 		        (list (plist-get info :latex-header)
 			      (and (not snippet?)
-			           (plist-get info :latex-header-extra)))
+			           (plist-get info :latex-header-extra))
+                              (and (not snippet?)
+                                   org-latex-use-sans
+                                   "\\renewcommand*\\familydefault{\\sfdefault}"))
+
 		        "")
              (org-latex-fontspec-to-string info)))))
       (if multi-lang
